88c7bb51cf67ea6d5a78807a1b1e5618bab76938
Jon Mooring <jonmooring@gmail.com>
2011-03-28 16:35:36 -0400

    Added some logic to begin processing the personality type assessment

--- assessments/views.py
+++ assessments/views.py
@@ -2,10 +2,25 @@ from django.contrib.auth.models import User
 from django.utils import simplejson as json
 from django.views.generic.simple import direct_to_template
 
-from osp.assessments import forms, models
+from osp.assessments.forms import PersonalityTypeForm, LearningStyleForm
+from osp.assessments.lib import jungian
+from osp.assessments.models import PersonalityTypeResult, LearningStyleResult
 
 def personality_type(request):
-    form = forms.PersonalityTypeForm()
+    if request.method == 'POST':
+        form = PersonalityTypeForm(request.POST)
+        if form.is_valid():
+            # Use the Jungian library to analyze the results of the assessment
+            analization = jungian.TypeAnalysis(form.cleaned_data, 4, 100)
+
+            # Extract important pieces of data from the analysis
+            answers = json.dumps(form.cleaned_data)
+            personality_type= ''.join(map(lambda x: x[0],
+                analization.computedScores))
+            first, second, third, fourth = map(lambda x: x[1],
+                analization.computedScores)
+    else:
+        form = PersonalityTypeForm()
     return direct_to_template(request, 'assessments/personality_type.html',
         {'form': form})
 
@@ -13,7 +28,7 @@ def personality_type_results(request):
     pass
 
 def learning_style(request):
-    form = forms.LearningStyleForm()
+    form = LearningStyleForm()
     return direct_to_template(request, 'assessments/learning_style.html',
         {'form': form})
 
