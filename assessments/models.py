d0f3388c60911ae958f705a408eb633338a86d42
Jon Mooring <jonmooring@gmail.com>
2011-03-28 13:07:13 -0400

    Added model for personality type assessment results. Added library to process answers to personality type assessment. Added personality type statements JSON data.

--- assessments/models.py
+++ assessments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class AssessmentResult(models.Model):
+    answers = models.TextField()
+    personality_type = models.CharField(max_length=255)
+    first_category_score = models.IntegerField()
+    second_category_score = models.IntegerField()
+    third_category_score = models.IntegerField()
+    fourth_category_score = models.IntegerField()
