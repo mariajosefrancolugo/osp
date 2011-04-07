92765c0d012e32211d416f34141b249ffe14c161
Jon Mooring <jonmooring@gmail.com>
2011-04-07 08:42:45 -0400

    Added index pages for both students and employees

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 from django.views.generic.simple import direct_to_template
 
+@login_required
 def index(request):
-    return direct_to_template(request, 'index.html', {})
+    if request.user.groups.filter(name='Employees'):
+        return direct_to_template(request, 'core/index_employee.html', {})
+    elif request.user.groups.filter(name='Students'):
+        return direct_to_template(request, 'core/index_student.html', {})
