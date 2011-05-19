a105478e7bf21bdfdad478e311e4af074b7c14ba
Jon Mooring <jon.mooring@cpcc.edu>
2011-05-19 17:46:23 -0400

    Altered the way the survey form is built. Made the survey form slightly prettier.

--- surveys/views.py
+++ surveys/views.py
@@ -1,19 +1,8 @@
-import os
-
-from django.utils import simplejson as json
 from django.views.generic.simple import direct_to_template
 
-from osp.surveys.utils import build_survey_form
-
-APP_ROOT = os.path.dirname(os.path.realpath(__file__))
-DATA_ROOT = os.path.join(APP_ROOT, 'data')
+from osp.surveys.forms import SurveyForm
 
 def survey(request):
-    f = file(os.path.join(DATA_ROOT, 'survey_questions.json'))
-    data = f.read()
-    data = json.loads(data)
-    f.close()
-
-    form = build_survey_form(data)
+    form = SurveyForm()
 
     return direct_to_template(request, 'surveys/survey.html', {'form': form})
