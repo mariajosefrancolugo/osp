c8a563f9e736432a576296055db8c637b35d2187
Jon Mooring <jon.mooring@cpcc.edu>
2011-05-23 13:32:15 -0400

    Fleshing out the survey app a bit more.

--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Result(models.Model):
+    student = models.ForeignKey(User)
+    date_taken = models.DateTimeField(auto_now_add=True)
+
+class Answer(models.Model):
+    result = models.ForeignKey(Result)
+    question = models.TextField()
+    answer = models.CharField(max_length=255)
--- surveys/views.py
+++ surveys/views.py
@@ -1,8 +1,21 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 from django.views.generic.simple import direct_to_template
 
+from osp.core.middleware.http import Http403
 from osp.surveys.forms import SurveyForm
 
+@login_required
 def survey(request):
-    form = SurveyForm()
+    if not request.user.groups.filter(name='Students'):
+        raise Http403
+
+    if request.method == 'POST':
+        form = SurveyForm(request.POST)
+        if form.is_valid():
+            print form.fields['question_1'].label
+            return redirect('profile:profile', request.user.id)
+    else:
+        form = SurveyForm()
 
     return direct_to_template(request, 'surveys/survey.html', {'form': form})
