656da7468a5f27c3e2e739306834fb26536dc56c
Jon Mooring <jon.mooring@cpcc.edu>
2011-05-31 17:29:18 -0400

    Optimized the API code to make fewer database calls

--- api/views.py
+++ api/views.py
@@ -11,14 +11,20 @@ def import_instructors(request):
     status = ['Starting instructor import...']
 
     if request.method == 'POST':
+        # Convert JSON data to Python object
+        data = json.loads(request.raw_post_data)
+
+        validate_credentials(request,
+                             settings.API_ALLOWED_HOSTS,
+                             settings.API_KEY,
+                             data[0]['api_key'])
+
         # Load users into local database using utility method
-        stats = load_users(request, 'instructors', ['Instructors', 'Employees'])
+        stats = load_users(data[0]['instructors'], ['Instructors', 'Employees'])
 
         status.append('Received %d instructor records' % stats[0])
         status.append('Updated %d user objects' % stats[1])
         status.append('Created %d user objects' % stats[2])
-        status.append('Deactivated %d user accounts' % stats[3])
-        status.append('Activated %d user accounts' % stats[4])
     else:
         status.append('Invalid request')
 
@@ -29,14 +35,20 @@ def import_students(request):
     status = ['Starting student import...']
 
     if request.method == 'POST':
+        # Convert JSON data to Python object
+        data = json.loads(request.raw_post_data)
+
+        validate_credentials(request,
+                             settings.API_ALLOWED_HOSTS,
+                             settings.API_KEY,
+                             data[0]['api_key'])
+
         # Load users into local database using utility method
-        stats = load_users(request, 'students', ['Students'])
+        stats = load_users(data[0]['students'], ['Students'])
 
         status.append('Received %d student records' % stats[0])
         status.append('Updated %d user objects' % stats[1])
         status.append('Created %d user objects' % stats[2])
-        status.append('Deactivated %d user accounts' % stats[3])
-        status.append('Activated %d user accounts' % stats[4])
     else:
         status.append('Invalid request')
 
@@ -55,44 +67,71 @@ def import_sections(request):
                              settings.API_KEY,
                              data[0].get('api_key', ''))
 
+        # Redefine data variable to the actual section list
+        data = data[0]['sections']
+
         # Let's keep a count of how many new and updated objects we have
         sections_updated = 0
         sections_created = 0
 
-        for s in data[0]['sections']:
-            # Finding (try) or creating (except) the section object
+        # Grab all existing sections and users as well as their associated
+        # objects from the database
+        all_sections = Section.objects.filter(
+            term=settings.CURRENT_TERM,
+            year=settings.CURRENT_YEAR
+        ).distinct().select_related()
+        all_users = UserProfile.objects.filter(
+            user__groups__name='Instructors'
+        ).distinct().select_related()
+
+        for s in data:
+            # Get the existing section object or create a new one
+            new_section = False
             try:
-                section = Section.objects.get(prefix=s['prefix'],
+                section = all_sections.filter(prefix=s['prefix'],
                                               number=s['number'],
-                                              section=s['section'],
-                                              term=s['term'],
-                                              year=s['year'])
-                sections_updated += 1
-            except:
+                                              section=s['section'])[0]
+            except IndexError:
                 section = Section(prefix=s['prefix'],
                                   number=s['number'],
                                   section=s['section'],
                                   term=s['term'],
                                   year=s['year'])
+                new_section = True
+
+            # Increment counter for appropriate operation type
+            if new_section:
                 sections_created += 1
+            else:
+                sections_updated += 1
 
-            # Find the related instructor user object(s) for the section
-            users = UserProfile.objects.filter(id_number__in=s['instructors'],
-                                               user__groups__name='Instructors')
-            instructors = [u.user for u in users]
+            # Only update metadata for section if changed
+            if (section.title != s['title']
+                or section.credit_hours != s['credit_hours']):
+                section.title= s['title']
+                section.credit_hours = s['credit_hours']
 
-            # Update metadata for section
-            section.title= s['title']
-            section.credit_hours = s['credit_hours']
+                section.save()
+            elif new_section:
+                section.save()
 
-            # Save the section
-            section.save()
-
-            # Assign instructor user objects to section
+            # Clear existing instructors and add up-to-date list of
+            # instructors to section
             section.instructors.clear()
-            [section.instructors.add(i) for i in instructors]
+            for i in s['instructors']:
+                instructor_exists = True
+                try:
+                    instructor = all_users.filter(id_number=i)[0].user
+                except IndexError:
+                    status.append('Instructor (ID number %s) '
+                                  'does not exist' % i)
+                    instructor_exists = False
 
-        status.append('Received %d section records' % len(data[0]['sections']))
+                if instructor_exists:
+                    section.instructors.add(instructor)
+
+
+        status.append('Received %d section records' % len(data))
         status.append('Updated %d section objects' % sections_updated)
         status.append('Created %d section objects' % sections_created)
     else:
@@ -113,79 +152,75 @@ def import_enrollments(request):
                              settings.API_KEY,
                              data[0].get('api_key', ''))
 
+        # Redefine data variable to actual enrollment list
+        data = data[0]['enrollments']
+
         # Let's keep a count of how many new and updated objects we have
         enrollments_updated = 0
         enrollments_created = 0
 
-        # Start a cached list of courses
-        sections = {}
+        # Grab all existing sections, users, and enrollments as well as
+        # their associated objects from the database
+        all_sections = Section.objects.filter(
+            term=settings.CURRENT_TERM,
+            year=settings.CURRENT_YEAR
+        ).distinct().select_related()
+        all_users = UserProfile.objects.filter(
+            user__groups__name='Instructors'
+        ).distinct().select_related()
+        all_enrollments = Enrollment.objects.filter(
+            section__term=settings.CURRENT_TERM,
+            section__year=settings.CURRENT_YEAR
+        ).distinct().select_related()
 
-        for e in data[0]['enrollments']:
-            # Find or create the enrollment object
+        for e in data:
+            section_exists = True
             try:
-                section = Section.objects.get(prefix=e['prefix'],
-                                              number=e['number'],
-                                              section=e['section'],
-                                              term=e['term'],
-                                              year=e['year'])
-                student = UserProfile.objects.get(
-                    id_number=e['student'],
-                    user__groups__name='Students'
-                ).user
-                enrollment = Enrollment.objects.get(section=section,
-                                                    student=student)
-                enrollments_updated += 1
-            except:
-                # Find the related section object for the section
-                #
-                # We keep a cache that we check first, and then hit the DB
-                # if we don't find it in the cache. If we don't find it in
-                # the DB, we throw an exception.
-                section_key = ('%s%s-%s-%s-%s' % (e['prefix'],
-                                                  e['number'],
-                                                  e['section'],
-                                                  e['year'],
-                                                  e['term']))
-
-                if (sections.has_key(section_key)):
-                    section = sections[section_key]
-                else:
-                    try:
-                        section = Section.objects.get(prefix=e['prefix'],
+                section = all_sections.objects.filter(prefix=e['prefix'],
                                                       number=e['number'],
-                                                      section=e['section'],
-                                                      term=e['term'],
-                                                      year=e['year'])
-                        sections[section_key] = section
-                    except:
-                        raise Exception('Section %s does not exist' %
-                            section_key)
+                                                      section=e['section'])[0]
+            except IndexError:
+                status.append('Section (%s%s-%s) does not exist' % (
+                    e['prefix'],
+                    e['number'],
+                    e['section'])
+                )
+                section_exists = False
 
-                # Find the related student user object for the enrollment
-                # Throw an exception if we can't find it
+            student_exists = True
+            try:
+                student = all_users.filter(id_number=e['student'])[0].user
+            except IndexError:
+                status.append('Student (ID number %s) '
+                              'does not exist' % e['student'])
+                student_exists = False
+
+            if section_exists and student_exists:
+                new_enrollment = False
                 try:
-                    student = UserProfile.objects.get(
-                        id_number=e['student'],
-                        user__groups__name='Students'
-                    ).user
-                except:
-                    raise Exception('Student %s does not exist' %
-                        e['student'])
+                    enrollment = all_enrollments.filter(section=section,
+                                                        student=student)[0]
+                except IndexError:
+                    enrollment = Enrollment(student=student, section=section)
+                    new_enrollment = True
 
-                enrollment = Enrollment(student=student, section=section)
-                enrollments_created += 1
+                if new_enrollment:
+                    enrollments_created += 1
+                else:
+                    enrollments_updated += 1
 
 
-            # Ensure that metadata for enrollment is up-to-date
-            enrollment.status = e['status']
+            # Only update metadata for enrollment if changed
+            if (enrollment.status != e['status']
+                or enrollment.grade != 'N/A'):
+                enrollment.status = e['status']
+                enrollment.grade = 'N/A'
 
-            # This will need to change later
-            enrollment.grade = 'N/A'
+                enrollment.save()
+            elif new_enrollment:
+                enrollment.save()
 
-            enrollment.save()
-
-        status.append('Received %d enrollment records' %
-                      len(data[0]['enrollments']))
+        status.append('Received %d enrollment records' % len(data))
         status.append('Updated %d enrollment objects' % enrollments_updated)
         status.append('Created %d enrollment objects' % enrollments_created)
     else:
