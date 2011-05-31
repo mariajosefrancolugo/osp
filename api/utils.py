06bd6349f0e78b569ae2d35e5ac80b5b280f53dd
Jon Mooring <jon.mooring@cpcc.edu>
2011-05-31 17:29:18 -0400

    Optimized the API code to make fewer database calls

--- api/utils.py
+++ api/utils.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.contrib.auth.models import User, Group
-from django.utils import simplejson as json
 
 from osp.core.models import UserProfile
 
@@ -16,7 +14,7 @@ def validate_credentials(request,
     elif request.META.has_key('REMOTE_ADDR'):
         host_received = request.META['REMOTE_ADDR']
     else:
-        raise Exception("Missing IP address")
+        raise Exception("Missing host IP address")
     if host_received.strip() in authorized_hosts:
         if authorized_key != provided_key:
             print provided_key
@@ -24,35 +22,32 @@ def validate_credentials(request,
             raise Exception("Invalid authorization key")
         return_status = True
     else:
-        raise Exception("Unauthorized IP address")
+        raise Exception("Unauthorized host IP address")
     return
 
-def load_users(request, user_type, groups):
-    # Convert JSON data to Python object
-    data = json.loads(request.raw_post_data)
-
-    validate_credentials(request,
-                         settings.API_ALLOWED_HOSTS,
-                         settings.API_KEY,
-                         data[0]['api_key'])
-
+def load_users(data, groups):
     # Store references to user groups
-    g = []
-    for group in groups:
-        g.append(Group.objects.get(name=group))
+    g = Group.objects.filter(name__in=groups)
 
     # Let's keep a count of how many new and updated objects we have
     # Also, how many user accounts we activate or deactivate
     users_updated = 0
     users_created = 0
-    users_deactivated = 0
-    users_activated = 0
+
+    # Grab all existing users and their associated objects in the
+    # specified groups from the database
+    all_users = UserProfile.objects.filter(
+        user__groups__name__in=groups
+    ).distinct().select_related()
 
     # Find or create user objects for each user
-    for u in data[0][user_type]:
+    for u in data:
+        # Check if the user has a user account
         if u['username']:
             username = u['username']
             has_account = True
+        # If the user does not have a user account, assign them a
+        # temporary username based on their ID number
         else:
             if 'Students' in groups:
                 username = str(u['id_number']) + 's'
@@ -60,53 +55,60 @@ def load_users(request, user_type, groups):
                 username = str(u['id_number']) + 'e'
             has_account = False
 
+        # Get the existing user object or create a new one
+        new_user = False
         try:
-            if 'Students' in groups:
-                group = 'Students'
-            elif 'Employees' in groups:
-                group = 'Employees'
-
-            user = UserProfile.objects.get(
-                id_number=u['id_number'],
-                user__groups__name=group
-            ).user
-
-            users_updated += 1
-        except:
+            user = all_users.filter(id_number=u['id_number'])[0].user
+        except IndexError:
             user = User.objects.create_user(username, u['email'])
-            users_created += 1
+            new_user = True
 
-        # Ensure that metadata for user is up-to-date
-        user.username = username
-        user.first_name = u['first_name']
-        user.last_name = u['last_name']
-        user.email = u['email']
+        # Increment counter for appropriate operation type
+        # Get or create the user profile object associated with the user
+        if new_user:
+            profile = UserProfile.objects.create(user=user)
+            users_created += 1
+        else:
+            profile = all_users.filter(user=user)[0]
+            users_updated += 1
+
+        # Check if anything changed before updating the user object
+        if (user.username != username
+            or user.first_name != u['first_name']
+            or user.last_name != u['last_name']
+            or user.email != u['email']):
+            user.username = username
+            user.first_name = u['first_name']
+            user.last_name = u['last_name']
+            user.email = u['email']
+
+            user.save()
+        elif new_user:
+            user.save()
 
         # Make sure that user is in the appropriate groups
-        user.groups.clear()
-        for group in g:
-            user.groups.add(group)
+        [user.groups.add(group)
+         for group in g
+         if group not in user.groups.all()]
 
         # Adjust user's account status as necessary
         if user.is_active and not u['is_active']:
             user.is_active = False
-            users_deactivated += 1
         elif not user.is_active and u['is_active']:
             user.is_active = True
-            users_activated += 1
 
-        profile = UserProfile.objects.get_or_create(user=user)[0]
-        profile.has_account = has_account
-        profile.id_number = u['id_number']
-        profile.phone_number = u['phone_number']
-        # profile.gpa = u['gpa']
+        # Check if anything changed before updating the profile object
+        if (profile.has_account != has_account
+            or profile.id_number != u['id_number']
+            or profile.phone_number != u['phone_number']):
+            profile.has_account = has_account
+            profile.id_number = u['id_number']
+            profile.phone_number = u['phone_number']
+            # profile.gpa = u['gpa']
 
-        user.save()
-        profile.save()
+            profile.save()
 
     # Return statistics on user account creation and modification
-    return (len(data[0][user_type]),
+    return (len(data),
             users_updated,
-            users_created,
-            users_deactivated,
-            users_activated)
+            users_created)
