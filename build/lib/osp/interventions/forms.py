7ce3022f22d06548b3257a3687b04e2479ac25e1
Jon Mooring <jon.mooring@cpcc.edu>
2011-06-02 17:03:43 -0400

    Added placeholder files for new directories. Removed build files.

--- build/lib/osp/interventions/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.conf import settings
-from osp.interventions.models import Intervention
-
-class InterventionForm(forms.ModelForm):
-    class Meta:
-        model = Intervention
-        exclude = ('students', 'date_submitted', 'staff', 'section',)
-        widgets = {'reasons': forms.CheckboxSelectMultiple}
