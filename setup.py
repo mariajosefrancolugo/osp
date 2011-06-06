4ee33d17ee505f3c4031d14018a5dcc3d11c589b
Jon Mooring <jon.mooring@cpcc.edu>
2011-06-06 11:40:32 -0400

    Added some classifiers to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,15 @@ setup(
                    'among high-risk students in higher education '
                    'institutions'),
     packages = find_packages(),
-    include_package_data = True
+    include_package_data = True,
+    zip_safe = False,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Education',
+        ('License :: OSI Approved :: GNU Library or Lesser General '
+         'Public License (LGPL)'),
+        'Programming Language :: Python',
+        'Programming Language :: JavaScript'
+    ]
 )
