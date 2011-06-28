8dd1d4d82db7c5b3831c6ba244f32a3ae38e3052
Jon Mooring <jon.mooring@cpcc.edu>
2011-06-28 11:01:53 -0400

    Refactored notifications (interventions/contacts) into its own app. Cleaned up Javascript for rosters app.

---
+++ osp/notifications/utils.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.core.mail import EmailMessage
+
+def email_user(from_user, to_user, subject, body):
+    """Used to intercept emails when in debug mode"""
+    subject = subject.rstrip('\n')
+    if settings.DEBUG:
+        subject = '(Orig: ' + to_user + ') ' + subject
+        for debug_user in settings.DEBUG_USERS:
+            message = EmailMessage(subject, body, from_user, [debug_user[1]])
+            message.content_subtype = 'html'
+            message.send()
+    else:
+        message = EmailMessage(subject, body, from_user, [to_user])
+        message.content_subtype = 'html'
+        message.send()
+    return
