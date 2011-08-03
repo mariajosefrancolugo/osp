af7c599aac869dafe45c5967d47f8e2f46d69264
Jon Mooring <jon.mooring@cpcc.edu>
2011-08-03 13:50:12 -0400

    Added UPGRADE document. Added counselor import to API. Added additional test data. Removed unused option from upgrade script.

--- osp/api/urls.py
+++ osp/api/urls.py
@@ -5,6 +5,7 @@ from osp.api import views
 urlpatterns = patterns('',
     (r'^instructor/import/', views.import_instructors, {},
         'import-instructors'),
+    (r'^counselor/import/', views.import_counselors, {}, 'import-counselors'),
     (r'^student/import/', views.import_students, {}, 'import-students'),
     (r'^section/import/', views.import_sections, {}, 'import-sections'),
     (r'^enrollment/import/', views.import_enrollments, {},
