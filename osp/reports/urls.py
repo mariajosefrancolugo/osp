2a2d76751d8ced6b6ccb85880d9eaaecba58156b
Jon Mooring <jon.mooring@cpcc.edu>
2011-08-17 09:41:12 -0400

    Fixed name of learning style report

--- osp/reports/urls.py
+++ osp/reports/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.defaults import *
 from osp.reports import views
 
 urlpatterns = patterns('',
-    (r'^learning/', views.learning_styles_report, {}, 'learning-styles-report'),
-    (r'^personality/', views.personality_type_report, {}, 'personality-type-report'),
+    (r'^learning-style/', views.learning_style_report, {},
+        'learning-style-report'),
+    (r'^personality-type/', views.personality_type_report, {},
+        'personality-type-report'),
 )
