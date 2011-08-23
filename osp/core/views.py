5442fc7052df985f08670aaa6bfeacf9b6512559
Jon Mooring <jon.mooring@cpcc.edu>
2011-08-23 10:24:26 -0400

    Added the ability to search by ID number

--- osp/core/views.py
+++ osp/core/views.py
@@ -1,3 +1,6 @@
+import re
+
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
@@ -6,6 +9,7 @@ from django.utils import simplejson as json
 from django.views.generic.simple import direct_to_template
 
 from osp.core.middleware.http import Http403
+from osp.core.models import UserProfile
 
 @login_required
 def index(request):
@@ -29,16 +33,25 @@ def search(request):
 
     query = request.GET.get('term', '')
 
-    # Ridiculously long query because we have to concatenate the first and
-    # last name fields in order to filter on full name
-    students = User.objects.filter(groups__name='Students').extra(
-        where=["""(`auth_user`.`first_name` LIKE %s
-                   OR `auth_user`.`last_name` LIKE %s
-                   OR concat(`auth_user`.`first_name`, ' ',
-                             `auth_user`.`last_name`) LIKE %s
-                   OR `auth_user`.`email` LIKE %s)"""],
-        params=['%s%%' % query] * 4
-    ).order_by('last_name', 'first_name').distinct()
+    students = []
+    if re.match(settings.ID_NUMBER_PATTERN, query):
+        try:
+            students = [UserProfile.objects.filter(
+                user__groups__name='Students', id_number=query
+            )[0].user]
+        except IndexError:
+            pass
+    if not students:
+        # Ridiculously long query because we have to concatenate the first and
+        # last name fields in order to filter on full name
+        students = User.objects.filter(groups__name='Students').extra(
+            where=["""(`auth_user`.`first_name` LIKE %s
+                       OR `auth_user`.`last_name` LIKE %s
+                       OR concat(`auth_user`.`first_name`, ' ',
+                                 `auth_user`.`last_name`) LIKE %s
+                       OR `auth_user`.`email` LIKE %s)"""],
+            params=['%s%%' % query] * 4
+        ).order_by('last_name', 'first_name').distinct()
 
     response = []
     for student in students:
