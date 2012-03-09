640631734c206c5d04015514d9adb03abae5e3fa
Julie Sparks <julie.sparks@cpcc.edu>
2012-03-09 13:40:04 -0500

    Allows multi select on reason when logging a vist; MODEL UPDATE (reason field converted to TextField in visit model); added new setting, VISIT_REASON_SEPARATOR

--- osp/visits/forms.py
+++ osp/visits/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from osp.visits.models import Visit
+from django.conf import settings
 
 class VisitForm(forms.ModelForm):
+    reason = forms.MultipleChoiceField(choices=settings.VISIT_REASON_CHOICES)
+
     class Meta:
         model = Visit
         exclude = ('student', 'submitter',)
 
+    def clean(self):
+        cleaned_data=self.cleaned_data
+        if cleaned_data.has_key('reason'):
+            # convert reason from list to string of comma separated values
+            reason_str = settings.VISIT_REASON_SEPARATOR.join(cleaned_data['reason'])
+            cleaned_data['reason'] = reason_str
+        return cleaned_data
