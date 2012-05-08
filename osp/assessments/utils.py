5e477c1110d04796209a9836b9157f981d8fe322
Julie Sparks <julie.sparks@cpcc.edu>
2012-05-08 14:27:17 -0400

    Added abstract AssessmentResult model, Assessment class with associated methods, and AssessmentForm - all for creating custom assessments; updated assessments utils.py to allow data_root to be passed in via parameter; updated assessments urls.py to check for urls for custom assessment apps

--- osp/assessments/utils.py
+++ osp/assessments/utils.py
@@ -5,8 +5,8 @@ from django.utils import simplejson as json
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
 DATA_ROOT = os.path.join(APP_ROOT, 'data')
 
-def load_json_data(json_file):
-    f = file(os.path.join(DATA_ROOT, json_file))
+def load_json_data(json_file, data_root=None):
+    f = file(os.path.join(data_root if data_root else DATA_ROOT, json_file))
     data = f.read()
     data = json.loads(data)
     f.close()
