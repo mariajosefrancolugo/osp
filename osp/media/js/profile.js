ae3c15871f6a62b026297da64e0be5e92fd3f66f
Edmund Quintero <edmund.quintero@cpcc.edu>
2012-11-01 13:35:54 -0400

    Fixed graphing function for personality type. Updates setup.py version 1.3.4

--- osp/media/js/profile.js
+++ osp/media/js/profile.js
@@ -48,7 +48,6 @@ $(".tab-window hr").remove();
 $(function(){
 
     $('#tabtoggle').click(function(){toggleTabs();});
-
     $('a').click(function(){
         if($(this).attr('href') == '#assessment-results') {
             styleTable();
@@ -200,75 +199,144 @@ function drawRCharts(){
         this.flag.animate({opacity: 0}, 300, function () {this.remove();});
     };
 
-
-    var r1Rect1 = r1.rect(25, 5, 400*personality_type_scores[0][1], 28, 3);
-    var r1Rect2 = r1.rect(25+400*personality_type_scores[0][1], 5, (400 - 400*personality_type_scores[0][1]), 28, 3);
+    if(personality_type_scores[0][0] == 'E'){
+        var r1Rect1 = r1.rect(25, 5, 400*personality_type_scores[0][1], 28, 3);
+        var r1Rect2 = r1.rect(25+400*personality_type_scores[0][1], 5, (400 - 400*personality_type_scores[0][1]), 28, 3);
+    }else{
+        var r1Rect1 = r1.rect(25, 5, 400*personality_type_scores[0][2], 28, 3);
+        var r1Rect2 = r1.rect(25+400*personality_type_scores[0][2], 5, (400 - 400*personality_type_scores[0][2]), 28, 3);
+    }
     r1Rect1.attr({fill: "#6D95F3", stroke: "#777"});
     r1Rect2.attr({fill: "#CCDAFB", stroke: "#777"});
     var value1 = personality_type_scores[0][1]
     value1 = value1*100;
     value1 = value1.toFixed(0);
-    value1 = value1+"%";
     var value2 = personality_type_scores[0][2]
     value2 = value2*100;
     value2 = value2.toFixed(0);
+    // Check for rounding errors and fixing smaller number
+    if( (Number(value1)+Number(value2)) > 100)
+    {
+        if(value1<value2){
+            value1=Number(value1)-1;
+        }
+        value2 = Number(value2)-1;
+    }
+    value1 = value1+"%";
     value2 = value2+"%";
+    if(personality_type_scores[0][0] == 'E'){
+        var r1Text = r1.text(50,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+        r1Text = r1.text(400,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+    }else{
+        var r1Text = r1.text(50,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+        r1Text = r1.text(400,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+    }
 
-    var r1Text = r1.text(50,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
-     r1Text = r1.text(400,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
-     // r1.path("M25, 40L425, 40");
-    // r1.text(225,38, "|")
 
-    var r2Rect1 = r2.rect(25, 5, 400*personality_type_scores[1][1], 28, 3);
-    var r2Rect2 = r2.rect(25+400*personality_type_scores[1][1], 5, (400 - 400*personality_type_scores[1][1]), 28, 3);
+    if(personality_type_scores[1][0] == 'N'){
+        var r2Rect1 = r2.rect(25, 5, 400*personality_type_scores[1][1], 28, 3);
+        var r2Rect2 = r2.rect(25+400*personality_type_scores[1][1], 5, (400 - 400*personality_type_scores[1][1]), 28, 3);
+    }else{
+        var r2Rect1 = r2.rect(25, 5, 400*personality_type_scores[1][2], 28, 3);
+        var r2Rect2 = r2.rect(25+400*personality_type_scores[1][2], 5, (400 - 400*personality_type_scores[1][2]), 28, 3);
+    }
     r2Rect1.attr({fill: "#6D95F3", stroke: "#777"});
     r2Rect2.attr({fill: "#CCDAFB", stroke: "#777"});
     value1 = personality_type_scores[1][1]
     value1 = value1*100;
     value1 = value1.toFixed(0);
-    value1 = value1+"%";
     value2 = personality_type_scores[1][2]
     value2 = value2*100;
     value2 = value2.toFixed(0);
+    // Check for rounding errors and fixing smaller number
+    if( (Number(value1)+Number(value2)) > 100)
+    {
+        if(value1<value2){
+            value1=Number(value1)-1;
+        }
+        value2 = Number(value2)-1;
+    }
+    value1 = value1+"%";
     value2 = value2+"%";
-    var r2Text = r2.text(50,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
-    r2Text = r2.text(400,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
-    //r2.path("M25, 40L425, 40");
-    //r2.text(225,38, "|")
+    if(personality_type_scores[1][0] == 'N'){
+        var r2Text = r2.text(50,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+        r2Text = r2.text(400,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+    }else{
+        var r2Text = r2.text(50,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+        r2Text = r2.text(400,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+    }
 
-    var r3Rect1 = r3.rect(25, 5, 400*personality_type_scores[2][1], 28, 3);
-    var r3Rect2 = r3.rect(25+400*personality_type_scores[2][1], 5, (400 - 400*personality_type_scores[2][1]), 28, 3);
+
+    if(personality_type_scores[2][0] == 'F'){
+        var r3Rect1 = r3.rect(25, 5, 400*personality_type_scores[2][1], 28, 3);
+        var r3Rect2 = r3.rect(25+400*personality_type_scores[2][1], 5, (400 - 400*personality_type_scores[2][1]), 28, 3);
+    }else{
+        var r3Rect1 = r3.rect(25, 5, 400*personality_type_scores[2][2], 28, 3);
+        var r3Rect2 = r3.rect(25+400*personality_type_scores[2][2], 5, (400 - 400*personality_type_scores[2][2]), 28, 3);
+    }
     r3Rect1.attr({fill: "#6D95F3", stroke: "#777"});
     r3Rect2.attr({fill: "#CCDAFB", stroke: "#777"});
     value1 = personality_type_scores[2][1]
     value1 = value1*100;
     value1 = value1.toFixed(0);
-    value1 = value1+"%";
     value2 = personality_type_scores[2][2]
     value2 = value2*100;
     value2 = value2.toFixed(0);
+    // Check for rounding errors and fixing smaller number
+    if( (Number(value1)+Number(value2)) > 100)
+    {
+        if(value1<value2){
+            value1=Number(value1)-1;
+        }
+        value2 = Number(value2)-1;
+    }
+    value1 = value1+"%";
     value2 = value2+"%";
-    var r3Text = r3.text(50,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
-    r3Text = r3.text(400,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
-    //r3.path("M25, 40L425, 40");
-    //r3.text(225,38, "|")
+    if(personality_type_scores[2][0] == 'F'){
+        var r3Text = r3.text(50,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+        r3Text = r3.text(400,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+    }else{
+        var r3Text = r3.text(50,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+        r3Text = r3.text(400,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+    }
 
-    var r4Rect1 = r4.rect(25, 5, 400*personality_type_scores[3][1], 28, 3);
-    var r4Rect2 = r4.rect(25+400*personality_type_scores[3][1], 5, (400 - 400*personality_type_scores[3][1]), 28, 3);
+    if(personality_type_scores[3][0] == 'J'){
+        var r4Rect1 = r4.rect(25, 5, 400*personality_type_scores[3][1], 28, 3);
+        var r4Rect2 = r4.rect(25+400*personality_type_scores[3][1], 5, (400 - 400*personality_type_scores[3][1]), 28, 3);
+    }else{
+        var r4Rect1 = r4.rect(25, 5, 400*personality_type_scores[3][2], 28, 3);
+        var r4Rect2 = r4.rect(25+400*personality_type_scores[3][2], 5, (400 - 400*personality_type_scores[3][2]), 28, 3);
+    }
     r4Rect1.attr({fill: "#6D95F3", stroke: "#777"});
     r4Rect2.attr({fill: "#CCDAFB", stroke: "#777"});
     value1 = personality_type_scores[3][1]
     value1 = value1*100;
     value1 = value1.toFixed(0);
-    value1 = value1+"%";
     value2 = personality_type_scores[3][2]
     value2 = value2*100;
     value2 = value2.toFixed(0);
+    // Check for rounding errors and fixing smaller number
+    if( (Number(value1)+Number(value2)) > 100)
+    {
+        if(value1<value2){
+            value1=Number(value1)-1;
+        }
+        value2 = Number(value2)-1;
+    }
+    value1 = value1+"%";
     value2 = value2+"%";
-    var r4Text = r4.text(50,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
-    r4Text = r4.text(400,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
-    //r4.path("M25, 40L425, 40");
-    //r4.text(225,38, "|")
+    if(personality_type_scores[3][0] == 'J'){
+        var r4Text = r4.text(50,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+        r4Text = r4.text(400,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+    }else{
+        var r4Text = r4.text(50,25,value2).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+        r4Text = r4.text(400,25,value1).attr({ "font-size": 20, "font-family": "Arial, Helvetica, sans-serif" });;
+    }
+
+    console.log(personality_type_scores[0][0]);
+    console.log(personality_type_scores[1][0]);
+    console.log(personality_type_scores[2][0]);
+    console.log(personality_type_scores[3][0]);
 
     if (personality_type_scores[0][0] == 'E') {
             $('#personality-type-chart-label1 span.left').addClass('highlight');
