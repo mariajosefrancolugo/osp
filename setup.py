42a13b2893e37d7f670708a85df458938e875395
Chris Emery <chris.emery@cpcc.edu>
2013-03-14 16:40:19 -0400

    update dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OSP',
-    version = '1.3.4',
+    version = '1.3.5',
     url = 'http://code.google.com/p/osp/',
     author = 'Central Piedmont Community College',
     description = ('Early warning system to improve retention rates '
